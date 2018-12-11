149b1dd7370604aee9d4ea26cec0554c503567fc
Aykut Asil <aykuttasil@gmail.com>
2018-12-11 21:48:05 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace android_management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
